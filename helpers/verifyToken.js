1ef66c2c87d7afc3ed2540a61acb363c3358ae22
Ben Schoenmakers <bpschoen@avvans.nl>
2018-03-29 13:10:54 +0200

    verify token with Bearer

--- helpers/verifyToken.js
+++ helpers/verifyToken.js
@@ -8,6 +8,7 @@ function verifyToken(req, res, next) {
         return res.status(403).send({ auth:false, message: "No token provided." });
     }
 
+    token = token.split('Bearer ').pop();
     jwt.verify(token, config.key, function(err, decoded) {
         if (err) {
             return res.status(500).send({ auth:false, message: "Failed to authenticate token." });
