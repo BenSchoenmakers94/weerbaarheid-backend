0f65d14c26c58fb168fdb93d2c9f50c59835e45e
Mark Havekes <mark.havekes@gmail.com>
2018-04-02 22:41:58 +0200

    websocket rooms

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var app = require('./app');
 var WebSocket = require('ws');
+var rooms = require("rooms");
 var http = require('http');
+var uuid = require('uuid');
 
 //initialize a simple http server
 const server = http.createServer(app);
@@ -9,6 +11,8 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
+    ws.id = uuid.v1();
+
     //connection is up, let's add a simple simple event
      ws.on('message', (message) => {
 
@@ -16,26 +20,33 @@ wss.on('connection', (ws) => {
         console.log('received: %s', message);
 
         const broadcastRegex = /^broadcast\:/i;
+        const joinRegex = /^join\:/i;
 
         if (broadcastRegex.test(message)) {
             message = message.replace(broadcastRegex, '');
 
             //send back the message to the other clients
-            wss.clients
-                .forEach(client => {
-                    if (client != ws) {
-                        client.send(`Hello, broadcast message -> ${message}`);
-                    }    
-                });
+            rooms.friends(ws.room, ws).forEach(client => {
+                client.send(`Hello, broadcast message -> ${message}`);
+            });
             
-        } else {
+        } else if(joinRegex.test(message)) {
+            room = message.replace(joinRegex, '');
+            rooms.join(room, ws);
+
+            rooms.friends(ws.room, ws).forEach(client => {
+                client.send(`Hello, client ${ws.id} has joined the room`);
+            });
+
+            ws.send(`Hello, you joined the room "${room}"`);
+        }
+        else {
             ws.send(`Hello, you sent -> ${message}`);
         }
     });
     //send immediatly a feedback to the incoming connection    
-    ws.send('Hi there, I am a WebSocket server');
+    ws.send('connected, first join a room by sending "join: <room>", then you can broadcast with "broadcast: <message>"');
 });
-
 var port = process.env.PORT || 3000;
 
 server.listen(port, function() {
