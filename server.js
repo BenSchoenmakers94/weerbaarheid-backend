1e3b9d35de3d47e1cf42746b47bdc2afebf5845a
Ben Schoenmakers <bpschoen@avvans.nl>
2018-04-02 21:32:32 +0200

    socket

--- server.js
+++ server.js
@@ -1,7 +1,43 @@
 var app = require('./app');
+var WebSocket = require('ws');
+var http = require('http');
+
+//initialize a simple http server
+const server = http.createServer(app);
+
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+    //connection is up, let's add a simple simple event
+     ws.on('message', (message) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+
+        const broadcastRegex = /^broadcast\:/i;
+
+        if (broadcastRegex.test(message)) {
+            message = message.replace(broadcastRegex, '');
+
+            //send back the message to the other clients
+            wss.clients
+                .forEach(client => {
+                    if (client != ws) {
+                        client.send(`Hello, broadcast message -> ${message}`);
+                    }    
+                });
+            
+        } else {
+            ws.send(`Hello, you sent -> ${message}`);
+        }
+    });
+    //send immediatly a feedback to the incoming connection    
+    ws.send('Hi there, I am a WebSocket server');
+});
 
 var port = process.env.PORT || 3000;
 
-var server = app.listen(port, function() {
+server.listen(port, function() {
     console.log('Express server listening on port ' + port);
 });
