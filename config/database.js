cc940eea50556409d4e96ea395464a3eafffce0a
Mark Havekes <mark.havekes@gmail.com>
2018-06-16 16:32:59 +0200

    fix

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose');
 var credentials = require('./credentials');
 
-mongoose.connect('mongodb://localhost:27017/weerbaarheid', 
+mongoose.connect('mongodb://' + credentials.userName + ':' + credentials.passWord + '@ds038547.mlab.com:38547/weerbaarheid',
     { keepAlive: 120 } , function(err) {
     if (err) {
         console.log(err);
+    } else {
+        console.log('Connection Successful!');
     }
 });
